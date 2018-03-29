ee29166f52b6ba9ad4dbaf3b9204f24bd717bed4
oleg yablonskiy <olegyablonskiy1993@gmail.com>
2018-03-29 02:30:43 +0000

    working

---
+++ js/index1.js
@@ -0,0 +1,23 @@
+const a = parseInt(prompt('Enter "a" coefficient, do not forget about minus, if it is there!, if there is no number just Enter 1'));
+const b = parseInt(prompt('Enter "b" coefficient, do not forget about minus, if it is there!, if there is no number just Enter 1'));
+const c = parseInt(prompt('Enter "c" coefficient, do not forget about minus, if it is there!'));
+
+const result = solveQuadr(a, b, c);
+document.write(result);
+
+const d = calcDisc;
+
+
+function solveQuadr(a, b, c) {
+   if (a === 0) {
+        document.write("It is not a Quadratic equation, it is linear equation");
+        return;
+    } else if (a !== 0) {
+        calcDisc = b*b - 4 * a * c;
+    }
+    const x1 =(-b + Math.sqrt(calcDisc)) / 2 * a;
+    const x2 =(-b - Math.sqrt(calcDisc)) / 2 * a;
+ 
+    return 'x1= '+ x1 + ' ' + 'x2= '+ x2;
+}
+   
