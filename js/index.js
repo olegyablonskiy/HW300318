46cab4d61adcbad636eb356d114ef9f8959b0d4f
oleg yablonskiy <olegyablonskiy1993@gmail.com>
2018-03-29 04:36:37 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -6,60 +6,20 @@ const result = solveQuadr(a, b, c);
 document.write(result);
 
 function solveQuadr(a, b, c) {
-   const d = calcDisc; 
-   const x1 = calcX1(-b + Math.sqrt(calcDisc)) / 2 * a;
-   const x2 = calcX2 (-b - Math.sqrt(calcDisc)) / 2 * a;
-   
+   const d = calcDisk; 
+   const x1 = (-b + Math.sqrt(d)) / (2 * a);
+   const x2 = (-b - Math.sqrt(d)) / (2 * a);
    return 'x1= '+ x1 + ' ' + 'x2= '+ x2;
 }
 
-if (a === 0) {
-//    function (stop) {
-    document.write("It is not a Quadratic equation, it is linear equation");
-//    return;
-//    }
-} else if (a !== 0) {
-    function calcDisc(a, b, c) {
-    calcDisc = b*b - 4 * a * c;
+function calcDisk (a, b, c) {
+    calcDisk = b** - 4 * a * c;
+    if (calcDisk < 0) {
+    return 'No roots';
+    } else if (calcDisk === 0) {
+        x = (-b / (2 * a));
+        return 'x= ' + x;
+    } else if (calcDisk > 0) {
+        return d;
     }
 }
-
-function calcX1 () {
-     (-b + Math.sqrt(calcDisc)) / 2 * a;
-}
-
-function calcX2 () {
-    let secondSolution = (-b - Math.sqrt(calcDisc)) / 2 * a;
-}*/
-
-
-
-
-
-
-/*    calcDisc = b*b - 4 * a * c;
-    x1 = (-b + Math.sqrt(calcDisc)) / 2 * a;
-    x2 = (-b - Math.sqrt(calcDisc)) / 2 * a;
-    return 'x1 = ' + x1 + 'x2 = ' + x2;
-}*/
-
-
-
-
-
-
-
-
-
-/*function calcDisc(a, b, c) {
-    calcDisc = b*b - 4 * a * c;
-}
-
-if (d === 0) {
-    x1 = -b / 2 * a;
-    document.write(result);
-} else if (d > 0) {
-    x1 = (-b + Math.sqrt(d)) / 2 * a;
-    x2 = (-b - Math.sqrt(d)) / 2 * a;
-    result = 'x1= ' + x1 + ' ' + 'x2= ' + x2;
-}*/
