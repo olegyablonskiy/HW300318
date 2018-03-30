bf62baa9ac1d51e36be01c014895935f9f0dd87a
oleg yablonskiy <olegyablonskiy1993@gmail.com>
2018-03-30 14:40:59 +0000

    final

--- js/index1.js
+++ js/index1.js
@@ -1,23 +1,23 @@
-const a = parseInt(prompt('Enter "a" coefficient, do not forget about minus, if it is there!, if there is no number just Enter 1'));
-const b = parseInt(prompt('Enter "b" coefficient, do not forget about minus, if it is there!, if there is no number just Enter 1'));
-const c = parseInt(prompt('Enter "c" coefficient, do not forget about minus, if it is there!'));
+const a = parseFloat(prompt('Enter "a" coefficient, do not forget about minus, if it is there!, if there is no number just Enter 1'));
+const b = parseFloat(prompt('Enter "b" coefficient, do not forget about minus, if it is there!, if there is no number just Enter 1'));
+const c = parseFloat(prompt('Enter "c" coefficient, do not forget about minus, if it is there!'));
 
 const result = solveQuadr(a, b, c);
 document.write(result);
 
 function solveQuadr(a, b, c) {
     if (a === 0) {
-        let x = -(c) / b;
+        let x = -c / b;
         return 'This is not a Quadratic equation, just a simple equation and result is' +' ' + 'x= ' + x;
     }
     
-    let calcDisc = (b * b) - (4 * a * c);
-    if (calcDisc < 0) {
+    const disk = b * b - 4 * a * c;
+    if (disk < 0) {
         return 'Has no real roots';
     }
     
-    let x1 = (-b + Math.sqrt(calcDisc)) / (2 * a);
-    let x2 = (-b - Math.sqrt(calcDisc)) / (2 * a);
+    const x1 = (-b + Math.sqrt(disk)) / (2 * a);
+    const x2 = (-b - Math.sqrt(disk)) / (2 * a);
     return 'x1= ' + x1 + ' ' + 'x2= ' + x2;
     
      /*  if ((a !== 0) && (b === 0) && (c !== 0)) {
